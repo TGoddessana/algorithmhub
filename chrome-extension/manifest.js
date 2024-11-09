e4a45038a74de61a1da0f9b72f41c3eb7c13f329
tgoddessana <twicegoddessana1229@gmail.com>
2024-11-09 21:42:23 +0900

    Refactor: remove firefox&side-panel code

--- chrome-extension/manifest.js
+++ chrome-extension/manifest.js
@@ -1,64 +1,51 @@
 import fs from 'node:fs';
-import deepmerge from 'deepmerge';
 
 const packageJson = JSON.parse(fs.readFileSync('../package.json', 'utf8'));
-
-const isFirefox = process.env.__FIREFOX__ === 'true';
-
-const sidePanelConfig = {
-  side_panel: {
-    default_path: 'side-panel/index.html',
-  },
-  permissions: ['sidePanel'],
-};
 
 /**
  * After changing, please reload the extension at `chrome://extensions`
  * @type {chrome.runtime.ManifestV3}
  */
-const manifest = deepmerge(
-  {
-    manifest_version: 3,
-    name: 'AlgorithmHub',
-    version: packageJson.version,
-    description: 'Automatically upload your algorithm-problem-solving history to GitHub. 🦾',
-    host_permissions: ['<all_urls>'],
-    permissions: ['storage', 'scripting', 'tabs', 'notifications'],
-    options_page: 'options/index.html',
-    background: {
-      service_worker: 'background.iife.js',
-      type: 'module',
-    },
-    action: {
-      default_popup: 'popup/index.html',
-      default_icon: 'icon-dark.png',
-    },
-    icons: {
-      128: 'icon-dark.png',
-    },
-    content_scripts: [
-      {
-        matches: ['https://*/*', '<all_urls>'],
-        js: ['content/index.iife.js'],
-      },
-      {
-        matches: ['https://*/*', '<all_urls>'],
-        js: ['content-ui/index.iife.js'],
-      },
-      {
-        matches: ['https://*/*', '<all_urls>'],
-        css: ['content.css'],
-      },
-    ],
-    devtools_page: 'devtools/index.html',
-    web_accessible_resources: [
-      {
-        resources: ['*.js', '*.css', '*.svg', 'icon-dark.png', 'icon-dark.png'],
-        matches: ['*://*/*'],
-      },
-    ],
+const manifest = {
+  manifest_version: 3,
+  name: 'AlgorithmHub',
+  version: packageJson.version,
+  description: 'Automatically upload your algorithm-problem-solving history to GitHub. 🦾',
+  host_permissions: ['<all_urls>'],
+  permissions: ['storage', 'scripting', 'tabs', 'notifications'],
+  options_page: 'options/index.html',
+  background: {
+    service_worker: 'background.iife.js',
+    type: 'module',
   },
-  !isFirefox && sidePanelConfig,
-);
+  action: {
+    default_popup: 'popup/index.html',
+    default_icon: 'icon-dark.png',
+  },
+  icons: {
+    128: 'icon-dark.png',
+  },
+  content_scripts: [
+    {
+      matches: ['https://*/*', '<all_urls>'],
+      js: ['content/index.iife.js'],
+    },
+    {
+      matches: ['https://*/*', '<all_urls>'],
+      js: ['content-ui/index.iife.js'],
+    },
+    {
+      matches: ['https://*/*', '<all_urls>'],
+      css: ['content.css'],
+    },
+  ],
+  devtools_page: 'devtools/index.html',
+  web_accessible_resources: [
+    {
+      resources: ['*.js', '*.css', '*.svg', 'icon-dark.png', 'icon-dark.png'],
+      matches: ['*://*/*'],
+    },
+  ],
+};
 
 export default manifest;
